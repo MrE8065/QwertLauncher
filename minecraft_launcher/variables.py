6d7024af9abe262d3caed172ddcb0d612eefb8c8
MrE8065 <102242666+MrE8065@users.noreply.github.com>
2025-02-10 10:14:42 +0100

    Update variables.py
    puto linux

--- minecraft_launcher/variables.py
+++ minecraft_launcher/variables.py
@@ -1,6 +1,4 @@
-import os
 import minecraft_launcher_lib as mll
 LAUNCHER_VERSION = ('1.0')
-USER = os.environ['USERNAME']
 MINECRAFT_DIRECTORY = mll.utils.get_minecraft_directory()
 CONFIG_JSON = f"{MINECRAFT_DIRECTORY}//config.json"
