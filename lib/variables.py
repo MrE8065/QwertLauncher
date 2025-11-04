55fafdd6b91de8db9568983208da72cd5e3089a6
MrE8065 <102242666+MrE8065@users.noreply.github.com>
2025-11-04 14:39:57 +0100

    Simplify variables

--- lib/variables.py
+++ lib/variables.py
@@ -1,15 +1,7 @@
 import os
+
 import minecraft_launcher_lib as mll
 
-IS_TESTING = True
-LAUNCHER_VERSION = '1.0'
-if IS_TESTING:
-  print("Modo de pruebas")
-  MINECRAFT_DIRECTORY = mll.utils.get_minecraft_directory().replace(".minecraft", ".qwert-test")
-  if not os.path.exists(MINECRAFT_DIRECTORY):
-    os.mkdir(MINECRAFT_DIRECTORY)
-  else:
-    pass
-else:
-  MINECRAFT_DIRECTORY = mll.utils.get_minecraft_directory()
-CONFIG_JSON = os.path.join(MINECRAFT_DIRECTORY, 'config.json')
+IS_TESTING = False  # Variable para determinar si usar el modo debug
+MINECRAFT_DIRECTORY = mll.utils.get_minecraft_directory()  # Ruta predeterminada de minecraft: "C:\Users\USUARIO\AppData\Roaming\.minecraft"
+CONFIG_JSON = os.path.join(MINECRAFT_DIRECTORY, 'config.json')  # Ruta del archivo de configuración
