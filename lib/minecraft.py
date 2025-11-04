d5944dce0092994b9437d6a316a5503048303246
MrE8065 <102242666+MrE8065@users.noreply.github.com>
2025-11-04 13:14:51 +0100

    Cleanup

--- lib/minecraft.py
+++ lib/minecraft.py
@@ -1,10 +1,9 @@
-import os
 import json
-import time
 import subprocess
 import uuid
+
 import minecraft_launcher_lib as mll
-from minecraft_launcher_lib.types import MinecraftOptions, CallbackDict
+from minecraft_launcher_lib.types import CallbackDict, MinecraftOptions
 
 import lib.variables as app_vars
 
@@ -51,80 +50,6 @@ def save_configs(nombre: str, ram: int):
     json.dump(config, file, indent=4)
 
 
-async def install_minecraft(menu_func):
-  global downloading
-  os.system('cls')
-  print("Selecciona la versión que quieres instalar (o escribe 0 para volver):")
-  minecraft_version = input('» ')
-
-  if minecraft_version == "0":
-    await menu_func()
-    return
-
-  if mll.utils.is_version_valid(minecraft_version, MINECRAFT_DIRECTORY):
-    os.system('cls')
-    mll.install.install_minecraft_version(minecraft_version, MINECRAFT_DIRECTORY, callback=callback)
-    downloading = False
-    time.sleep(0.2)  # Para evitar solapamiento de textos
-    print(f"\n» Versión {minecraft_version} instalada correctamente")
-    time.sleep(1.5)
-    await menu_func()
-  else:
-    downloading = False
-    print(f"\nERROR: {minecraft_version} no es una versión válida")
-    time.sleep(1.5)
-    await install_minecraft(menu_func)
-
-
-async def install_forge(menu_func):
-  global downloading
-  os.system('cls')
-  print('Introduce la versión de Minecraft con Forge (o pulsa Enter para volver):')
-  forge_ver = input('» ')
-
-  if forge_ver == "":
-    await menu_func()
-    return
-
-  try:
-    forge = mll.forge.find_forge_version(forge_ver)
-    mll.forge.install_forge_version(forge, MINECRAFT_DIRECTORY, callback=callback)
-    downloading = False
-    time.sleep(0.2)
-    print("\n◈ Forge instalado correctamente ◈")
-    time.sleep(1.5)
-    await menu_func()
-  except Exception as e:
-    downloading = False
-    print(f"\nERROR: Versión no válida - {str(e)}")
-    time.sleep(10)
-    await install_forge(menu_func)
-
-
-async def install_fabric(menu_func):
-  global downloading
-  os.system('cls')
-  print('Introduce la versión de Minecraft con Fabric (o escribe 0 para volver):')
-  fabric_ver = input('» ')
-
-  if fabric_ver == "0":
-    await menu_func()
-    return
-
-  if mll.fabric.is_minecraft_version_supported(fabric_ver):
-    mll.fabric.install_fabric(fabric_ver, MINECRAFT_DIRECTORY, callback=callback)
-    downloading = False
-    time.sleep(0.2)
-    print("\n◈ Fabric instalado correctamente ◈")
-    time.sleep(1.5)
-    await menu_func()
-  else:
-    downloading = False
-    print("\nERROR: Versión no soportada por Fabric")
-    time.sleep(2)
-    await install_fabric(menu_func)
-
-
 def install_version(version: str, release_type: str, callback: CallbackDict):
   """
   Instala la versión especificada
@@ -151,6 +76,8 @@ def install_version(version: str, release_type: str, callback: CallbackDict):
 
 
 async def play_mine(version):
+  """Llama al proceso de inicio del juego"""
+
   with open(app_vars.CONFIG_JSON, 'r', encoding='utf-8') as file:
     data = json.load(file)
 
